4b66961097cbee9a33d6f9a17cdefa8ca5b0fe1e
russddy <nrusso1993@gmail.com>
2020-08-11 00:37:03 -0400

    initial commit

---
+++ Player.gd
@@ -0,0 +1,29 @@
+extends AnimatedSprite
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	play();
+	set_position(Vector2(100,500))	
+	pass # Replace with function body.
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	
+	if Input.is_key_pressed(KEY_RIGHT):
+		position += Vector2(150 * delta,0) 
+		self.flip_h = false
+	if Input.is_key_pressed(KEY_LEFT):
+		position += Vector2(-150 * delta,0)
+		self.flip_h = true
+	if(!Input.is_key_pressed(KEY_LEFT) && !Input.is_key_pressed(KEY_RIGHT)):
+		set_animation('idle')
+	else:
+		set_animation('walk')
+	pass
